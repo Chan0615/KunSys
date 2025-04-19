8054e0391cb29715dcafbf66421037cb74385e9b
ChAn <3194212409@qq.com>
2025-04-19 19:42:11 +0800

    模块：登录 功能：登录已做好

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+
+
+@app.route('/')
+def hello_index():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
+
+
+@app.route('/admin', methods=['POST'])
+def admin_index():
+    ## 登录后获取用户名，密码，验证码，之后校验，一致则登录，并记录信息
+    print(request.form)
+    username = request.form.get('username')
+    password = request.form.get('password')
+    captcha = request.form.get('captcha')
+    print(f'username:{username} password:{password} captcha:{captcha}')
+
+    ## 登录成功后
+    return render_template('auth/admin.html')
+
+
+if __name__ == '__main__':
+    app.run()
